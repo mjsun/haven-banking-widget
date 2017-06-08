1c7d838916b007ef75cc9fb65b5e29c10cae5ce7
msun <mingjie@havenlife.com>
2017-06-08 17:53:34 -0400

    update main js

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-// main entry point
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { BankModule } from './app/banking/bank.module';
-import { BeneModule } from './app/beneficiary/bene.module';
-
-platformBrowserDynamic().bootstrapModule(BankModule);
-platformBrowserDynamic().bootstrapModule(BeneModule);
