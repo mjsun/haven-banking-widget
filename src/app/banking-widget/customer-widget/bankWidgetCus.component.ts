36ae7abaf372fe741f8b39e6cf9dd3114bbbccf8
msun <mingjie@havenlife.com>
2017-06-27 11:19:23 -0400

    added validation

--- src/app/banking-widget/customer-widget/bankWidgetCus.component.ts
+++ src/app/banking-widget/customer-widget/bankWidgetCus.component.ts
@@ -21,7 +21,12 @@ export class BankWidgetCusComponent implements OnInit {
         {value: 'saving', display: 'Saving'}
     ];
     public bankInfoForm: FormGroup;
-
+    public displayError: any = {
+        accountType: null,
+        routing_number: null,
+        accountNumber: null,
+        bankName: null
+    };
     constructor(private  bankService: BankService, private windowService: WindowService, private router: Router, private fb: FormBuilder) {
         let parentController = windowService.nativeWindow.controller || windowService.nativeWindow.parent.controller || {customer : {applicationPolicy: {policyNumber: '1'}}};
         this.policyNumber ='110000001' || parentController.customer.applicationPolicy.policyNumber;
@@ -54,16 +59,85 @@ export class BankWidgetCusComponent implements OnInit {
         ];
 
         this.bankInfoForm = fb.group({
-            'accountType': ['checking', Validators.required],
-            'routing_number': [null, Validators.required],
+            'accountType': [null, Validators.required],
+            'routing_number':  [null, Validators.required],
             'accountNumber': [null, Validators.required],
-            'bankName': [null, Validators.required]
+            'bankName': [, Validators.required]
+        });
+
+        this.bankInfoForm.valueChanges.subscribe(data => {
+            // console.log('Form changes', data.accountType);
+
+            if(data.accountType === null) {
+                this.displayError.accountType = 'Please choose account type';
+            }
+            else {
+                this.displayError.accountType = null;
+            }
+
+            if(data.routing_number === null) {
+                this.displayError.routing_number = 'Please input routing number';
+            }
+            else if(data.routing_number.toString().length !== 9) {
+                // this.displayError.routing_number = 'Please input 9 digit routing number';
+            }
+            else if(!this.checkSum(data.routing_number)) {
+                this.displayError.routing_number = 'Please input valid routing number';
+            }
+            else {
+                this.displayError.routing_number = null;
+            }
+
+            if(data.accountNumber === null) {
+                this.displayError.accountNumber = 'Please input account number';
+            }
+            else {
+                this.displayError.accountNumber = null;
+            }
+
+
+            if(data.bankName == null || data.bankName.replace(/\s/g, '') === '') {
+                this.displayError.bankName = 'Please input account number';
+            }
+            else {
+                this.displayError.bankName = null;
+            }
+
         });
 
     };
 
-    submitForm(value: any){
-        console.log(value);
+    validateRoutingLength(routing_number: number) {
+        if(routing_number && routing_number.toString().length !== 9) {
+            this.displayError.routing_number = 'Please input 9 digit routing number';
+        }
+    }
+
+    validateForm() {
+        for(let field in this.displayError) {
+            if(this.displayError[field] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkSum(routing: number) {
+        let routingArr = routing.toString().split('');
+        let res: number = 0;
+        for(let i = 0; i < 9; i++) {
+            let currNumber = Number(routingArr[i]);
+            if(i === 0 || i === 3 || i === 6) {
+                res += currNumber * 3;
+            }
+            if(i === 1 || i === 4 || i === 7) {
+                res += currNumber * 7;
+            }
+            if(i === 2 || i === 5 || i === 8) {
+                res += currNumber * 1;
+            }
+        }
+        return (res % 10) ? false : true
     }
 
     ngOnInit() {
@@ -73,13 +147,15 @@ export class BankWidgetCusComponent implements OnInit {
             });
     }
 
-
     updateBankInfo() {
-        this.bankService.postBankInfo(this.policyNumber, this.bankInfo)
-            .subscribe(bankInfo => {
-                this.router.navigate(['complete']);
-                return true;
-            });
+        if(this.validateForm()) {
+            console.log('Sending info.')
+            this.bankService.postBankInfo(this.policyNumber, this.bankInfo)
+                .subscribe(bankInfo => {
+                    this.router.navigate(['complete']);
+                    return true;
+                });
+        }
     }
 }
 
